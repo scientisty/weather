de37d9e50f61bb9bce4e8f75baf2357fe87dbbf9
Spencer Mathews <spencermathews@users.noreply.github.com>
2018-03-20 01:24:48 -0700

    Implement working geolocate() function using promises
    Move spinner logic out of geolocate.

--- nws/js/geolocate.js
+++ nws/js/geolocate.js
@@ -1,43 +1,53 @@
-if ("geolocation" in navigator) {
-  overlay.startOverlay(); // initializes overlay and spinner
-  console.log("geolocation is available");
-  navigator.geolocation.getCurrentPosition(success, error, options);
-} else {
-  console.log("geolocation IS NOT available");
+/*
+ * Returns a promise for the user's position.
+ * 
+ * Inspired by https://www.codeschool.com/blog/2016/01/08/es2015-and-the-geolocation-api/,
+ * and to a lesser extent https://gist.github.com/varmais/74586ec1854fe288d393.
+ * 
+ * @returns {Promise<string>} position 
+ */
+function getPosition() {
+  return new Promise((resolve, reject) => {
+    if ("geolocation" in navigator) {
+      console.log("geolocation is available");
+      navigator.geolocation.getCurrentPosition(success, error, options);
+    } else {
+      console.log("geolocation IS NOT available");
+    }
+
+    function success(position) {
+      console.log(
+        "Your current position is:" +
+          "\nlatitude:  " +
+          position.coords.latitude +
+          "\nlongitude: " +
+          position.coords.longitude +
+          "\naccuracy:  " +
+          position.coords.accuracy +
+          " m" +
+          "\naltitude:  " +
+          position.coords.altitude +
+          " +/- " +
+          position.coords.altitudeAccuracy +
+          "\nheading:   " +
+          position.coords.heading +
+          "\nspeed:     " +
+          position.coords.speed +
+          "\ntimestamp: " +
+          position.timestamp
+      );
+      resolve(position); // returns a Promise object that is resolved with value
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+      reject(err); // returns a promise object that is rejected
+    }
+
+    var options = {
+      enableHighAccuracy: true, // default: false
+      timeout: 5000, // default: Infinity
+      maximumAge: 0 // default: 0
+    };
+  });
 }
-
-function success(position) {
-  console.log(
-    "Your current position is:" +
-      "\nlatitude:  " +
-      position.coords.latitude +
-      "\nlongitude: " +
-      position.coords.longitude +
-      "\naccuracy:  " +
-      position.coords.accuracy +
-      " m" +
-      "\naltitude:  " +
-      position.coords.altitude +
-      " +/- " +
-      position.coords.altitudeAccuracy +
-      "\nheading:   " +
-      position.coords.heading +
-      "\nspeed:     " +
-      position.coords.speed +
-      "\ntimestamp: " +
-      position.timestamp
-  );
-
-  var latitude = position.coords.latitude;
-  var longitude = position.coords.longitude;
-}
-
-function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-}
-
-var options = {
-  enableHighAccuracy: true, // default: false
-  timeout: 5000, // default: Infinity
-  maximumAge: 0 // default: 0
-};
